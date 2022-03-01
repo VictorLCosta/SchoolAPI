2b524a717f3dc1b1aa7ae01a8a3e3e08866a1599
VictorLCosta <victorlc2019@outlook.com>
2022-03-01 10:57:52 -0300

    Services - Student service

--- SchoolApi.Services/Services/StudentService.cs
+++ SchoolApi.Services/Services/StudentService.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using SchoolApi.Data.Transactions;
 using SchoolApi.Domain.DTO.Student;
+using SchoolApi.Domain.Entities;
 using SchoolApi.Services.Interfaces;
 
 namespace SchoolApi.Services.Services
@@ -10,35 +12,45 @@ namespace SchoolApi.Services.Services
     public class StudentService : IStudentService
     {
         private readonly IUow _uow;
+        private readonly IMapper _mapper;
 
-        public StudentService(IUow uow)
+        public StudentService(IUow uow, IMapper mapper)
         {
             _uow = uow;
+            _mapper = mapper;
         }
 
-        public Task<bool> Delete(Guid id)
+        public async Task<bool> Delete(Guid id)
         {
-            throw new NotImplementedException();
+            return await _uow.Students.RemoveAsync(id);
         }
 
-        public ValueTask<IEnumerable<StudentDto>> GetAll()
+        public async ValueTask<IEnumerable<StudentDto>> GetAll()
         {
-            throw new NotImplementedException();
+            var students = await _uow.Students.GetAllAsync();
+            
+            return _mapper.Map<IEnumerable<StudentDto>>(students);
         }
 
         public async ValueTask<StudentDto> GetById(Guid id)
         {
-            throw new NotImplementedException();
+            var student = await _uow.Students.GetByIdAsync(id);
+
+            return _mapper.Map<StudentDto>(student);
         }
 
-        public Task<CreateStudentResultDto> Post(CreateStudentDto student)
+        public async Task<CreateStudentResultDto> Post(CreateStudentDto student)
         {
-            throw new NotImplementedException();
+            var studentDb = _mapper.Map<Student>(student);
+
+            return _mapper.Map<CreateStudentResultDto>(await _uow.Students.AddAsync(studentDb));
         }
 
-        public Task<UpdateStudentResultDto> Put(UpdateStudentDto student)
+        public async Task<UpdateStudentResultDto> Put(UpdateStudentDto student)
         {
-            throw new NotImplementedException();
+            var studentDb = _mapper.Map<Student>(student);
+
+            return _mapper.Map<UpdateStudentResultDto>(await _uow.Students.UpdateAsync(studentDb));
         }
     }
 }
